eff21ab68744cde6b9e73d81700a7d1af73650f9
sohaibusmani135 <sohaibusmani135@gmail.com>
2026-07-24 11:10:27 +0500

    Add Vercel serverless function for backend API

---
+++ api/index.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'backend'))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+django.setup()
+
+from fastapi_service.main import app
